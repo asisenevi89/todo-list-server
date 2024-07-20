f086fa6a0069764527161adcaf06d67d4dc330d7
Asitha Seneviratne <asisenevi@gmail.com>
2024-07-21 01:52:58 +0530

    Added cors changes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import todoRoutes from './routes/todoRoutes';
 import commonRoutes from './routes';
 
@@ -7,6 +8,9 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+  origin: process.env.FRONT_END_URL
+}));
 
 const port = process.env.PORT || 9000;
 
